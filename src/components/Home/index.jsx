f95ec49830a55fd6e8b6bb5e1c38f3fa8564de4c
Karina Kurmanova <620179@csinow.edu>
2024-08-27 20:05:47 -0400

    message

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -11,7 +11,20 @@ const HomePage = () => {
                 <Gallery/>
                 <Buttons/>
                 <h1 className="title">Innovating with Technology</h1>
-               
+                <div class="image-container">
+  <div class="image-box">
+  <img src="/img/js.jpg" alt="js" class="circled-img" />  
+    <p>Description 1</p>
+  </div>
+  <div class="image-box">
+  <img src="/img/python.jpg" alt="python" class="circled-img" />
+    <p>Description 2</p>
+  </div>
+  <div class="image-box">
+  <img src="/img/react.jpg" alt="react" class="circled-img" />
+    <p>Description 3</p>
+  </div>
+</div>
                 <Footer/>
 
             </div>          
